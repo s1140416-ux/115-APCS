58d5e2c5eb0dc012015bcd6015bafde80701c3ad
s1140416-ux <s1140416@dmhs.kh.edu.tw>
2026-09-15 11:55:55 +0800

    Create a003光占卜.cpp

---
+++ a003光占卜.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int M, D, S;
+
+    cin >> M >> D;
+    S = (M * 2 + D ) % 3
+    if(S == 0)
+       cout << "普通" << end1;
+    if(S == 1)
+          cout << "吉" << end1;
+    if(S == 2)
+          cout << "大吉" << end1;
+	return 0;
+}
